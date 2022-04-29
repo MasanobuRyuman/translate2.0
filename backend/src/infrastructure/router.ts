20cfad9bc1c057a97d6e86d90bcc3509104cdf6c
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-04-29 15:26:28 +0900

    認証テスト

--- backend/src/infrastructure/router.ts
+++ backend/src/infrastructure/router.ts
@@ -1,23 +1,15 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/require-await */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-var-requires */
-import  express = require('express')
+import express = require('express')
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const session = require('express-session')
 import { AppDataSource } from './data-source'
-import { Users } from '../domain/entities/Users'
 import { QuestionController } from '../interfaces/controllers/QuestionController'
 import { UserController } from '../interfaces/controllers/UserController'
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
 
 const app = express()
-const user = new Users()
+
 AppDataSource.initialize()
   .then(async () => {
     console.log(
@@ -29,56 +21,65 @@ AppDataSource.initialize()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(passport.initialize())
 
+app.use(session({
+  secret: 'crackalackindafaafa',
+  resave: false,
+  saveUninitialized: false,
+  cookie : { secure : false, maxAge : (4 * 60 * 60 * 1000) }, // 4 hours})); 
+}));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(cookieParser());
+
 passport.use(
   new LocalStrategy(async function (username, password, done) {
     const userController = new UserController(AppDataSource)
     const result = await userController.signIn(username, password)
-    if (result.success == true){
-      done(null,result)
-    }else{
-      done(null,false)
+    if (result.success == true) {
+      done(null, "nobu")
+    } else {
+      done(null, false)
     }
   })
 )
 
 passport.serializeUser((user, done) => {
-  console.log("serializeUserですよ〜")
+  console.log('serializeUserですよ〜')
   done(null, user)
 })
-
-passport.deserializeUser(( user, done) => {
+passport.deserializeUser(function(user,done) {
   console.log("deserializeUserですよ〜")
-    return done(null, user);
+  return done(null,user)
 });
-
-app.use(
-  session({
-    secret: 'keyboard_cat',
-    resave: false,
-    saveUninitialized: false,
-  })
-)
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(passport.authenticate('session'));
 
 app.listen(3000, () => {
   console.log('Start on port 3000.')
 })
 
-app.get('/', async (req, res) => {
-  res.send(req.user)
-})
+app.get('/',function(req:any, res, next) {
+  console.log(req.user)
+});
 
-app.post('/api/signUp/', async (req, res) => {
+app.post('/api/signUp/', async (req:any, res) => {
   const userController = new UserController(AppDataSource)
   const name = req.body.username
   const password = req.body.password
   const result = await userController.signUp(name, password)
+  var user = {
+    username: name,
+    password:password
+  };
+  req.login(user, function(err) {
+    
+  });
   res.send(result)
 })
 
-app.post('/api/signIn/', passport.authenticate('local'), async (req, res) => {res.send("成功")})
+app.post('/api/signIn/', passport.authenticate('local',{
+  session:true
+}), async (req, res) => {
+  res.send("成功")
+})
 
 app.get('/api/find/:id', async (req, res) => {
   const questionController = new QuestionController(AppDataSource)
@@ -110,6 +111,6 @@ app.post('/api/delete/:id', async (req) => {
   const result = await questionController.deleteQuestion(questionId)
 })
 
-app.post('/test',async(req,res) =>{
-  res.send(req.body.username)
+app.post('/test', async (req, res) => {
+  res.send(req.body)
 })

e5a954f9dfb2639305a3bac3d8cfcfb935e36f45
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-05-01 17:10:45 +0900

    2

--- frontend/src/components/ui/organisms/translateArea.tsx
+++ frontend/src/components/ui/organisms/translateArea.tsx
@@ -1,55 +1,75 @@
-import {Box} from '@mui/material'
-import React,{useState} from  'react'
+import { Box } from '@mui/material'
+import React, { useState } from 'react'
 
-import {DefaultButton,DefaultTextArea,H4} from '../atoms'
+import { DefaultButton, DefaultTextArea, H4 } from '../atoms'
 
-export const TranslateArea = ()=>{
-  const [rightTranslateFiled,setRightTranslateFiled] = useState("英語")
-  const [leftTranslateFiled,setLeftTranslateFiled] = useState("日本語")
+export const TranslateArea = () => {
+  const [rightTranslateFiled, setRightTranslateFiled] = useState('英語')
+  const [leftTranslateFiled, setLeftTranslateFiled] = useState('日本語')
 
-  const ChangeTranslate = ()=>{
-    const rightTranslate:string = rightTranslateFiled
-    const leftTranslate:string = leftTranslateFiled
+  const ChangeTranslate = () => {
+    const rightTranslate: string = rightTranslateFiled
+    const leftTranslate: string = leftTranslateFiled
     setRightTranslateFiled(leftTranslate)
     setLeftTranslateFiled(rightTranslate)
   }
-  return(
+  return (
     <div>
-      <Box sx={{
-        width : 800,
-        height : 400,
-        border : "solid",
-        borderColor : "red",
-      }}>
-        <Box sx={{
-          display : "flex",
-        }}>
-          <Box sx={{
-          }}>
-            <H4 sx={{
-              textAlign : "center",
-            }}>{rightTranslateFiled}</H4>
-            <DefaultTextArea Width={300} Height={200}/>
+      <Box
+        sx={{
+          width: 700,
+          height: 400,
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+          }}
+        >
+          <Box sx={{}}>
+            <H4
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {rightTranslateFiled}
+            </H4>
+            <DefaultTextArea Width={300} Height={200} />
           </Box>
-          <Box sx={{
-            mr:0,
-            ml:"auto",
-          }}>
-            <H4 sx={{
-              textAlign : "center",
-            }}>{leftTranslateFiled}</H4>
-            <DefaultTextArea Width={300} Height={200}/>
+          <Box
+            sx={{
+              mr: 0,
+              ml: 'auto',
+            }}
+          >
+            <H4
+              sx={{
+                textAlign: 'center',
+              }}
+            >
+              {leftTranslateFiled}
+            </H4>
+            <DefaultTextArea Width={300} Height={200} />
           </Box>
         </Box>
-        <DefaultButton onClick={()=>ChangeTranslate()} sx={{
-          display: "block",
-          m: "auto",
-        }}>入れ替え</DefaultButton>
-        <DefaultButton sx={{
-          display: "block",
-          m:"auto",
-          mt: 2,
-        }}>保存</DefaultButton>
+        <DefaultButton
+          onClick={() => ChangeTranslate()}
+          sx={{
+            display: 'block',
+            m: 'auto',
+          }}
+        >
+          入れ替え
+        </DefaultButton>
+        <DefaultButton
+          sx={{
+            display: 'block',
+            m: 'auto',
+            mt: 2,
+          }}
+        >
+          保存
+        </DefaultButton>
       </Box>
     </div>
   )

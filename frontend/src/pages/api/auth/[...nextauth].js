e6684b10cb518a0e422aa552ede594fd146c4fbf
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-05-08 12:57:53 +0900

    認証完了

--- frontend/src/pages/api/auth/[...nextauth].js
+++ frontend/src/pages/api/auth/[...nextauth].js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
@@ -7,6 +8,7 @@ const options = {
   // 認証プロバイダー
   providers: [
     CredentialsProvider({
+      id: "signIn",
       name: 'ログイン',
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
@@ -19,19 +21,21 @@ const options = {
       }
     }),
     CredentialsProvider({
+      id: "signUp",
       name: '新規登録',
+      async authorize(credentials, req) {
+        const result = await axios.get('http://localhost:3001/api/find/1')
+        console.log("kita")
+        console.log(result.data)
+        return (null)
+      },
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: {  label: "Password", type: "password" }
       },
-      async authorize(credentials, req) {
-        return ({
-          name:"test"
-        })
-      }
     }),
   ],
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_URL,
 }
 
 export default (req, res) => NextAuth(req, res, options)

1cc302221b802c84bad232172c7fa9e65d05b566
ryumanmasanobu <22011376mr@tama.ac.jp>
2022-05-20 17:01:08 +0900

    mysqlの設定変更

--- backend/src/infrastructure/data-source.ts
+++ backend/src/infrastructure/data-source.ts
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
+  host: '127.0.0.1',
   port: 3306,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
